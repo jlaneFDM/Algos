0a6ec3c10ad5404a40e8d351eb23632238ccd371
John <jlane7590@yahoo.ca>
2021-11-26 12:18:49 -0500

    corrected comment on line 16 in SortOMatic

--- src/com/selectionsort/www/SortOMatic.java
+++ src/com/selectionsort/www/SortOMatic.java
@@ -1,38 +1,36 @@
 package com.selectionsort.www;
+
 import java.util.Arrays;
 
 public class SortOMatic {
 
 	public static void main(String[] args) {
 		int[] unsorted = { 14, 33, 27, 10, 35, 19, 42, 44 };
-		//int[] unsorted = { 5,1,4,2,3 };
-				
+		// int[] unsorted = { 5,1,4,2,3 };
+
 		System.out.println("Result: " + sort(unsorted));
-		
+
 	}
-	
-	public static String sort(int [] arr) {
-	   //real world implementation
-	   //Arrays.sort(unsortedNumbers);
-	   
-	   //reinvent the wheel implementation:
-	   
-			for (int i = 0; i < arr.length; i++) {
-			
+
+	public static String sort(int[] arr) {
+		// real world implementation
+		// Arrays.sort(unsorted);
+
+		// reinvent the wheel implementation:
+
+		for (int i = 0; i < arr.length; i++) {
+
 			for (int j = 0; j < arr.length; j++) {
 				if (arr[i] < arr[j]) {
-					var memo = arr[i];
 					
+					var memo = arr[i];
 					arr[i] = arr[j];
 					arr[j] = memo;
-					
 				}
 			}
 		}
-	   
+
 		return Arrays.toString(arr);
 	}
-	
-	
 
 }
